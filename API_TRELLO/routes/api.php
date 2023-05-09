f19679a03f66a9ca6c4afbe5ad4046c2a18220ee
ladyxlazarus <yasminajouau@gmail.com>
2023-05-09 18:23:22 +0200

    fix api route

--- API_TRELLO/routes/api.php
+++ API_TRELLO/routes/api.php
@@ -1,19 +1,15 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TrelloController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/', function () {
+    return redirect()->route('boards.show'); // Redirigir a la página de tarjetas
 });
+
+Route::get('/boards', [TrelloController::class, 'getBoards'])->name('boards.show');
+Route::get('/cards/create/{boardId}', [TrelloController::class, 'showCreateCardForm'])->name('cards.create');
+Route::post('/cards', [TrelloController::class, 'storeCard'])->name('cards.store');
+Route::get('/cards/{id}/edit', [TrelloController::class, 'edit'])->name('cards.edit');
+Route::put('/cards/{id}', [TrelloController::class, 'update'])->name('cards.update');
+Route::get('/cards/{boardId}', [TrelloController::class, 'index'])->name('cards.index');
