1e3da12ce9b0efc4d45e6fb92c6e4fca1d99f6f9
ladyxlazarus <yasminajouau@gmail.com>
2023-05-16 13:10:44 +0200

    add marvel api

--- API_TRELLO/resources/views/auth/selectApi.blade.php
+++
@@ -1,3 +0,0 @@
-  <h1>Selecciona una API</h1>
-  <a href="{{ route('boards.show') }}" class="btn btn-primary">Trello API</a>
-  <a href="{{ route('movies.indexShows') }}" class="btn btn-primary">TMDB API</a>
